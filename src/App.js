dc044b66dbee0de505c04d5a15865011753c6ca0
Mijat <nmijatn@gmail.com>
2021-08-24 00:36:55 +0200

    text changed on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          top top topcina.
+          Mala menjaj
         </p>
         <a
           className="App-link"
